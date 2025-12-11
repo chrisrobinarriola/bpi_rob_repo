cb102c982225401b6c5310a0a9adf68c0765e4c3
Christopher Robin L. Arriola <crlarriola@headoffice.corp.bpi.com.ph>
2025-12-11 14:48:02 +0800

    M2_Activity2 init

---
+++ M2_Activity2/src/bpi/training/Main.java
@@ -0,0 +1,11 @@
+package bpi.training;
+
+public class Main {
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+	
+	
+}
---
+++ M2_Activity2/src/bpi/training/WhiteBoard.java
@@ -0,0 +1,8 @@
+package bpi.training;
+
+public class WhiteBoard {
+	int addNumbers(int addend1, int addend2) {
+		int result = addend1 + addend2;
+		return result;
+	}
+}
