1ff8921f566ed32b2c9dd9930dd27022bec89193
Christopher Robin L. Arriola <crlarriola@headoffice.corp.bpi.com.ph>
2025-12-11 14:35:11 +0800

    M1_Activity4 init

---
+++ M1_Activity4/src/bpi/training/Main.java
@@ -0,0 +1,27 @@
+package bpi.training;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter your age: ");
+		int int1 = scanner.nextInt();
+		
+		String result = "";
+		
+		if(int1 < 18) {
+			result = "minor";
+		} else if(int1 >= 18 && int1 < 60) {
+		  result = "adult";
+		} else {
+			result = "senior";
+		}
+		
+		System.out.println(result);
+
+	}
+	
+}
